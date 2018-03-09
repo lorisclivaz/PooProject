65a69bf98d4c6ec4f1d103aca026dc414c42324f
Loris_Clivaz <Loris_Clivaz@Loris_Clivaz-HP>
2018-03-09 13:17:42 +0100

    try1

---
+++ PooProject/src/Test/Test.java
@@ -0,0 +1,25 @@
+/*Test
+* Author: Clivaz Loris
+* Date creation: 9 mars 2018
+* 
+*/
+/**
+ * 
+ */
+package Test;
+
+/**
+ * @author Loris_Clivaz
+ *
+ */
+public class Test {
+
+	
+	public static void main(String[] args) {
+
+		System.out.println("Salut vivian");
+		
+		
+	}
+
+}
