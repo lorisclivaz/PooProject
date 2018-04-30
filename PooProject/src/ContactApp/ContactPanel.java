90107f77ac9d4a5353e2b8e5ce4180cd349406cf
vbrid <vbrid@PC-VIVIAN>
2018-04-30 14:01:56 +0200

    MAJ 30.04.2018 14:01

---
+++ PooProject/src/ContactApp/ContactPanel.java
@@ -0,0 +1,41 @@
+/*Vivian Bridy
+* Date creation: 30 avr. 2018
+* 
+* On définit la couleur #EFEFEF comme étant la couleur de fond de base
+* 
+*/
+/**
+ * 
+ */
+package ContactApp;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import javax.swing.JPanel;
+
+import Panels.MenuH1Panel;
+
+/**
+ * @author Loris_Clivaz
+ *
+ */
+public class ContactPanel extends JPanel
+{
+	private String titre = "Contacts";
+	MenuH1Panel menuh1panel = new MenuH1Panel(titre);
+	ListePanel listepanel = new ListePanel();
+	
+	
+	public ContactPanel() {
+		// TODO Auto-generated constructor stub
+		this.setPreferredSize(new Dimension(480, 40));
+		this.setBackground(Color.decode("#EFEFEF")); 
+		
+		//On affiche titre H1 dans le panel UP
+		this.setLayout(new BorderLayout());
+		this.add(menuh1panel, BorderLayout.NORTH);
+		this.add(listepanel);
+		
+	}
+}
