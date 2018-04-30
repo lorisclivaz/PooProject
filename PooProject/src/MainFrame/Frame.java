de4915e6f66e460d72e31a6a4a634eb0a79daa4c
Loris_Clivaz <Loris_Clivaz@Loris_Clivaz-HP>
2018-04-30 10:21:32 +0200

    Création de la fenetre @loris

---
+++ PooProject/src/MainFrame/Frame.java
@@ -0,0 +1,21 @@
+/*
+* Frame
+* Author: Clivaz Loris
+* Date creation: 30 avr. 2018
+* 
+*/
+/**
+ * 
+ */
+package MainFrame;
+
+import javax.swing.JFrame;
+
+/**
+ * @author Loris_Clivaz
+ *
+ */
+public class Frame extends JFrame
+{
+
+}
