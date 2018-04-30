e34a0295fc3694957a122daafa4c8ae398d3fe54
Loris_Clivaz <Loris_Clivaz@Loris_Clivaz-HP>
2018-04-30 10:09:37 +0200

    test25

--- PooProject/src/Test/Test.java
+++ PooProject/src/Test/Test.java
@@ -17,9 +17,9 @@ public class Test {
 	
 	public static void main(String[] args) {
 
-		System.out.println("Salut bg2");
+		System.out.println("Vivian le cop1");
 		
-		
+		System.out.println("salut c'est loris");
 	}
 
 }
