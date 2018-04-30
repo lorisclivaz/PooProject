d39b2916a18a744b9384e5229cc4ae0843349ba9
Loris_Clivaz <Loris_Clivaz@Loris_Clivaz-HP>
2018-04-30 18:11:14 +0200

    Finalisation de la page d'accueil @loris

--- PooProject/src/Images/ImageFond.java
+++ PooProject/src/Images/ImageFond.java
@@ -35,7 +35,6 @@ public void paintComponent(Graphics g){
 		try {
 	    	
 	        Image img = ImageIO.read(new File("images/background/imagefond.jpg"));
-	        g.drawImage(img, 0, 0, this);
 	        g.drawImage(img, 0, 0, this.getWidth(), this.getHeight(), this);
 	        
 	      } catch (IOException e) {
