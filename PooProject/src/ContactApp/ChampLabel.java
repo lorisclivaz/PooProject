5edcfe6ea5622e7c794e9e00db67e4356ca7c7c3
vbrid <vbrid@PC-VIVIAN>
2018-05-01 15:12:27 +0200

    MAJ DétailsContact @Vivian

---
+++ PooProject/src/ContactApp/ChampLabel.java
@@ -0,0 +1,24 @@
+/*
+* Author : Vivian Bridy
+* Date creation : 1 mai 2018
+*/
+package ContactApp;
+
+import java.awt.Dimension;
+import java.awt.Font;
+
+import javax.swing.JLabel;
+
+public class ChampLabel extends JLabel {
+	
+	Font globalFontH2 = new Font("2.TimesRoman ",Font.BOLD,20);
+	
+	public ChampLabel(String nom,String type) {
+		// TODO Auto-generated constructor stub
+		super(nom);
+		//Design des champs
+		this.setPreferredSize(new Dimension(200,50));
+		this.setFont(globalFontH2);
+	
+	}
+}
