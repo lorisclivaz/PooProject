d1a14f223a139ace7eb1fe1069c2f1a65515e7e9
Loris_Clivaz <Loris_Clivaz@Loris_Clivaz-HP>
2018-05-08 12:49:32 +0200

    @loris ajout de l'heure

--- PooProject/src/Panels/UpPanel.java
+++ PooProject/src/Panels/UpPanel.java
@@ -12,7 +12,10 @@ package Panels;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.util.Date;
 
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 /**
@@ -21,15 +24,24 @@ import javax.swing.JPanel;
  */
 public class UpPanel extends JPanel{
 
+	Date madate = new Date();
+	JLabel date = new JLabel();
+	
 	
 
 		//Constructeur qui définit la taille du panel ou il y aura la batterie l'heures ect...
 	
 		public UpPanel() 
 		{
-			this.setLayout(new BorderLayout());
+			this.setLayout(new FlowLayout(100,215,20));
 			this.setPreferredSize(new Dimension(480, 50));
 			this.setBackground(Color.BLACK);
+			
+			date.setSize(100, 100);
+			date.setText(madate.getHours()+" : "+ madate.getMinutes()+" ");
+			date.setForeground(Color.WHITE);
+			
+			this.add(date);
 		}
 	}
 
