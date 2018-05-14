5904b6aad892c2c87b3a4d7aa449019f78eab9fc
vbrid <vbrid@PC-VIVIAN>
2018-05-14 11:43:59 +0200

    Mail essai @Vivan

---
+++ PooProject/src/MailApp/SendMailSSL.java
@@ -0,0 +1,55 @@
+/*
+* Author : Vivian Bridyh
+* Date creation : 14 mai 2018
+*/
+package MailApp;
+
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class SendMailSSL {
+
+	public static void main(String[] args) {
+
+		final String username = "vbridy2@gmail.com";
+		final String password = "tiloupcomputer1";
+
+		Properties props = new Properties();
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.smtp.host", "smtp.gmail.com");
+		props.put("mail.smtp.port", "587");
+
+		Session session = Session.getInstance(props,
+		  new javax.mail.Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(username, password);
+			}
+		  });
+
+		try {
+
+			Message message = new MimeMessage(session);
+			message.setFrom(new InternetAddress("vbridy2@gmail.com"));
+			message.setRecipients(Message.RecipientType.TO,
+				InternetAddress.parse("vbridy3@gmail.com"));
+			message.setSubject("Testing Subject");
+			message.setText("Dear Mail Crawler,"
+				+ "\n\n No spam to my email, please!");
+
+			Transport.send(message);
+
+			System.out.println("Done");
+
+		} catch (MessagingException e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
