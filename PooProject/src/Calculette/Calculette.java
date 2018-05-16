586cafef0be2a5b0f07f6c0ded30e8321c498ef0
Loris_Clivaz <Loris_Clivaz@Loris_Clivaz-HP>
2018-05-16 22:04:52 +0200

    @loris Calculette Création et implémentation du code

--- PooProject/src/Calculette/Calculette.java
+++ PooProject/src/Calculette/Calculette.java
@@ -3,7 +3,9 @@ package Calculette;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.Font;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener; 
 import javax.swing.BorderFactory;
@@ -11,230 +13,282 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-  
-public class Calculette extends JPanel {
-  private JPanel container = new JPanel();
-  private JPanel david = new JPanel();
-  //Tableau stockant les éléments à afficher dans la calculatrice
-  String[] tab_string = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "0", ".", "=", "C", "+", "-", "*", "/"};
-  
-  //Un bouton par élément à afficher
-  JButton[] tab_button = new JButton[tab_string.length];
-  private JLabel ecran = new JLabel();
-  private Dimension dim = new Dimension(50, 40);
-  private Dimension dim2 = new Dimension(50, 31);
-  private double chiffre1;
-  private boolean clicOperateur = false, update = false;
-  private String operateur = "";
-  
-  public Calculette(){
-	  
+
+import Images.IconBase;
+import Panels.PanelCalculette;
+
+public class Calculette extends JPanel
+{
+
+	PanelCalculette panelcalculette = new PanelCalculette("Calculatrice", "Calculatrice");
+	IconBase point = new IconBase("images/icones/points.png", 50,50);
+
+
+	JPanel center = new JPanel();
+	JPanel bas = new JPanel();
+
+	//Tableau qui stocke les éléments de la calculatrice
+	String [] tab_string = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "0", ".", "=",
+			"C", "+", "-", "*", "/"};
+
+
+	//bouton
+	JButton [] tab_button = new JButton[tab_string.length];
+
+	//JLabel ecran ou les chiffres seront notés
+	private JLabel ecran = new JLabel();
+
+	//variable qui va permettre de faire les calcules
+	private double chiffre1;
+
+	private String operateur;
+
+	//choisir la dimension des boutons
+	private Dimension dim = new Dimension(60, 60);
+	private Dimension dim2 = new Dimension(90, 90);
+
+	private boolean clicOperateur = false, update = false;
+
+	public Calculette()
+	{
+
+		//choisir la dimension du panel de base
 		this.setPreferredSize(new Dimension(480, 40));
-		//On initialise le conteneur avec tous les composants
-	  initComposant();
-	  
-	  //On ajoute le conteneur
-	  david.add(container, BorderLayout.CENTER);
-    this.add(david, BorderLayout.CENTER);
-    this.setVisible(true);
-  }
-      
-  private void initComposant(){
-	  
-    //On définit la police d'écriture à utiliser
-    Font police = new Font("Arial", Font.BOLD, 20);
-    ecran = new JLabel("0");
-    ecran.setFont(police);
-    
-    //On aligne les informations à droite dans le JLabel
-    ecran.setHorizontalAlignment(JLabel.RIGHT);
-    ecran.setPreferredSize(new Dimension(220, 20));
-    JPanel operateur = new JPanel();      
-    operateur.setPreferredSize(new Dimension(55, 225));
-    JPanel chiffre = new JPanel();
-    chiffre.setPreferredSize(new Dimension(165, 225));
-    JPanel panEcran = new JPanel();
-    panEcran.setPreferredSize(new Dimension(220, 30));
+		this.setLayout(new BorderLayout());
+		
+		//on rajoute le panel par defaut des apps dans le north du panel de base
+		this.add(panelcalculette, BorderLayout.NORTH);
 
-    //On parcourt le tableau initialisé
-    //afin de créer nos boutons
-    for(int i = 0; i < tab_string.length; i++){
-      tab_button[i] = new JButton(tab_string[i]);
-      tab_button[i].setPreferredSize(dim);
-      switch(i){
-        //Pour chaque élément situé à la fin du tableau
-        //et qui n'est pas un chiffre
-        //on définit le comportement à avoir grâce à un listener
-        case 11 :
-          tab_button[i].addActionListener(new EgalListener());
-          chiffre.add(tab_button[i]);
-          break;
-        case 12 :
-          tab_button[i].setForeground(Color.red);
-          tab_button[i].addActionListener(new ResetListener());
-          operateur.add(tab_button[i]);
-          break;
-        case 13 :
-          tab_button[i].addActionListener(new PlusListener());
-          tab_button[i].setPreferredSize(dim2);
-          operateur.add(tab_button[i]);
-          break;
-        case 14 :
-          tab_button[i].addActionListener(new MoinsListener());
-          tab_button[i].setPreferredSize(dim2);
-          operateur.add(tab_button[i]);
-          break;	
-        case 15 :	
-          tab_button[i].addActionListener(new MultiListener());
-          tab_button[i].setPreferredSize(dim2);
-          operateur.add(tab_button[i]);
-          break;
-        case 16 :
-          tab_button[i].addActionListener(new DivListener());
-          tab_button[i].setPreferredSize(dim2);
-          operateur.add(tab_button[i]);
-          break;
-        default :
-          //Par défaut, ce sont les premiers éléments du tableau
-          //donc des chiffres, on affecte alors le bon listener
-          chiffre.add(tab_button[i]);
-          tab_button[i].addActionListener(new ChiffreListener());
-          break;
-      }
-    }
-    panEcran.add(ecran);
-    panEcran.setBorder(BorderFactory.createLineBorder(Color.black));
-    container.add(panEcran, BorderLayout.NORTH);
-    container.add(chiffre, BorderLayout.CENTER);
-    container.add(operateur, BorderLayout.EAST);
-  }
+		//on met le layout du centre pour l'ordre des panels
+		center.setLayout(new BorderLayout());
+		center.setBackground(Color.RED);
+		
+		//panel bas 
+		bas.setLayout(new FlowLayout());
+		bas.setSize(new Dimension(480, 40));
+		bas.add(point);
+		//mets tous les composants dans le panel center
+		Composant();
 
-  //Méthode permettant d'effectuer un calcul selon l'opérateur sélectionné
-  private void calcul(){
-    if(operateur.equals("+")){
-      chiffre1 = chiffre1 + 
-            Double.valueOf(ecran.getText()).doubleValue();
-      ecran.setText(String.valueOf(chiffre1));
-    }
-    if(operateur.equals("-")){
-      chiffre1 = chiffre1 - 
-            Double.valueOf(ecran.getText()).doubleValue();
-      ecran.setText(String.valueOf(chiffre1));
-    }          
-    if(operateur.equals("*")){
-      chiffre1 = chiffre1 * 
-            Double.valueOf(ecran.getText()).doubleValue();
-      ecran.setText(String.valueOf(chiffre1));
-    }     
-    if(operateur.equals("/")){
-      try{
-        chiffre1 = chiffre1 / 
-              Double.valueOf(ecran.getText()).doubleValue();
-        ecran.setText(String.valueOf(chiffre1));
-      } catch(ArithmeticException e) {
-        ecran.setText("0");
-      }
-    }
-  }
+		this.add(center, BorderLayout.CENTER);
+		this.add(bas, BorderLayout.SOUTH);
 
-  //Listener utilisé pour les chiffres
-  //Permet de stocker les chiffres et de les afficher
-  class ChiffreListener implements ActionListener {
-    public void actionPerformed(ActionEvent e){
-      //On affiche le chiffre additionnel dans le label
-      String str = ((JButton)e.getSource()).getText();
-      if(update){
-        update = false;
-      }
-      else{
-        if(!ecran.getText().equals("0"))
-          str = ecran.getText() + str;
-      }
-      ecran.setText(str);
-    }
-  }
+	}
 
-  //Listener affecté au bouton =
-  class EgalListener implements ActionListener {
-    public void actionPerformed(ActionEvent arg0){
-      calcul();
-      update = true;
-      clicOperateur = false;
-    }
-  }
 
-  //Listener affecté au bouton +
-  class PlusListener implements ActionListener {
-    public void actionPerformed(ActionEvent arg0){
-      if(clicOperateur){
-        calcul();
-        ecran.setText(String.valueOf(chiffre1));
-      }
-      else{
-        chiffre1 = Double.valueOf(ecran.getText()).doubleValue();
-        clicOperateur = true;
-      }
-      operateur = "+";
-      update = true;
-    }
-  }
+	private void Composant() 
+	{
+		//police des boutons
+		Font police2 = new Font("Arial", Font.BOLD, 50);
 
-  //Listener affecté au bouton -
-  class MoinsListener implements ActionListener {
-    public void actionPerformed(ActionEvent arg0){
-      if(clicOperateur){
-        calcul();
-        ecran.setText(String.valueOf(chiffre1));
-      }
-      else{
-        chiffre1 = Double.valueOf(ecran.getText()).doubleValue();
-        clicOperateur = true;
-      }
-      operateur = "-";
-      update = true;
-    }
-  }
+		//On définit la police d'écriture à utiliser
+		Font police = new Font("Arial", Font.BOLD, 40);
+		ecran = new JLabel("0");
+		ecran.setFont(police);
+		//On aligne les informations à droite dans le JLabel
+		ecran.setHorizontalAlignment(JLabel.CENTER);
+		ecran.setPreferredSize(new Dimension(220, 40));
+		JPanel operateur = new JPanel();      
+		operateur.setPreferredSize(new Dimension(200, 225));
+		JPanel chiffre = new JPanel();
+		chiffre.setLayout(new GridLayout(4, 3,5,5));
+		chiffre.setPreferredSize(new Dimension(200, 225));
+		JPanel panEcran = new JPanel();
+		panEcran.setPreferredSize(new Dimension(220, 60));
 
-  //Listener affecté au bouton *
-  class MultiListener implements ActionListener {
-    public void actionPerformed(ActionEvent arg0){
-      if(clicOperateur){
-        calcul();
-        ecran.setText(String.valueOf(chiffre1));
-      }
-      else{
-        chiffre1 = Double.valueOf(ecran.getText()).doubleValue();
-        clicOperateur = true;
-      }
-      operateur = "*";
-      update = true;
-    }
-  }
+		//On parcourt le tableau 
+		//afin de créer nos boutons avec la couleur de fond changé
+		//ainsi que la police de caractère
+		
+		for(int i = 0; i < tab_string.length; i++){
+			tab_button[i] = new JButton(tab_string[i]);
+			tab_button[i].setPreferredSize(dim);
+			tab_button[i].setBackground(Color.darkGray);
+			tab_button[i].setFont(police2);
+			switch(i){
+			
+			//Pour chaque élément situé à la fin du tableau
+			//et qui n'est pas un chiffre
+			//on définit le comportement à avoir grâce à un listener
+			case 11 :
+				tab_button[i].addActionListener(new EgalListener());
+				chiffre.add(tab_button[i]);
+				tab_button[i].setBackground(Color.DARK_GRAY);
+				break;
+			case 12 :
+				tab_button[i].setForeground(Color.red);
+				tab_button[i].addActionListener(new ResetListener());
+				tab_button[i].setPreferredSize(dim2);
+				operateur.add(tab_button[i]);
+				tab_button[i].setBackground(Color.GRAY);
+				break;
+			case 13 :
+				tab_button[i].addActionListener(new PlusListener());
+				tab_button[i].setPreferredSize(dim2);
+				operateur.add(tab_button[i]);
+				tab_button[i].setBackground(Color.GRAY);
+				break;
+			case 14 :
+				tab_button[i].addActionListener(new MoinsListener());
+				tab_button[i].setPreferredSize(dim2);
+				operateur.add(tab_button[i]);
+				tab_button[i].setBackground(Color.GRAY);
+				break;	
+			case 15 :	
+				tab_button[i].addActionListener(new MultiListener());
+				tab_button[i].setPreferredSize(dim2);
+				operateur.add(tab_button[i]);
+				tab_button[i].setBackground(Color.GRAY);
+				break;
+			case 16 :
+				tab_button[i].addActionListener(new DivListener());
+				tab_button[i].setPreferredSize(dim2);
+				operateur.add(tab_button[i]);
+				tab_button[i].setBackground(Color.GRAY);
+				break;
+			default :
+				//Par défaut, ce sont les premiers éléments du tableau
+				//donc des chiffres, on affecte alors le bon listener
+				chiffre.add(tab_button[i]);
+				tab_button[i].addActionListener(new ChiffreListener());
+				break;
+			}
+		}
+		panEcran.add(ecran);
+		panEcran.setBorder(BorderFactory.createLineBorder(Color.black));
+		center.add(panEcran, BorderLayout.NORTH);
+		center.add(chiffre, BorderLayout.CENTER);
+		center.add(operateur, BorderLayout.SOUTH);
+	}
 
-  //Listener affecté au bouton /
-  class DivListener implements ActionListener {
-    public void actionPerformed(ActionEvent arg0){
-      if(clicOperateur){
-        calcul();
-        ecran.setText(String.valueOf(chiffre1));
-      }
-      else{
-        chiffre1 = Double.valueOf(ecran.getText()).doubleValue();
-        clicOperateur = true;
-      }
-      operateur = "/";
-      update = true;
-    }
-  }
+	//Méthode permettant d'effectuer un calcul selon l'opérateur sélectionné
+	private void calcul(){
+		if(operateur.equals("+")){
+			chiffre1 = chiffre1 + 
+					Double.valueOf(ecran.getText()).doubleValue();
+			ecran.setText(String.valueOf(chiffre1));
+		}
+		if(operateur.equals("-")){
+			chiffre1 = chiffre1 - 
+					Double.valueOf(ecran.getText()).doubleValue();
+			ecran.setText(String.valueOf(chiffre1));
+		}          
+		if(operateur.equals("*")){
+			chiffre1 = chiffre1 * 
+					Double.valueOf(ecran.getText()).doubleValue();
+			ecran.setText(String.valueOf(chiffre1));
+		}     
+		if(operateur.equals("/")){
+			try{
+				chiffre1 = chiffre1 / 
+						Double.valueOf(ecran.getText()).doubleValue();
+				ecran.setText(String.valueOf(chiffre1));
+			} catch(ArithmeticException e) {
+				ecran.setText("0");
+			}
+		}
+	}
 
-  //Listener affecté au bouton de remise à zéro
-  class ResetListener implements ActionListener {
-    public void actionPerformed(ActionEvent arg0){
-      clicOperateur = false;
-      update = true;
-      chiffre1 = 0;
-      operateur = "";
-      ecran.setText("");
-    }
-  }      
+	//Listener utilisé pour les chiffres
+	//Permet de stocker les chiffres et de les afficher
+	class ChiffreListener implements ActionListener {
+		public void actionPerformed(ActionEvent e){
+			//On affiche le chiffre additionnel dans le label
+			String str = ((JButton)e.getSource()).getText();
+			if(update){
+				update = false;
+			}
+			else{
+				if(!ecran.getText().equals("0"))
+					str = ecran.getText() + str;
+			}
+			ecran.setText(str);
+		}
+	}
+
+	//Listener affecté au bouton =
+	class EgalListener implements ActionListener {
+		public void actionPerformed(ActionEvent arg0){
+			calcul();
+			update = true;
+			clicOperateur = false;
+		}
+	}
+
+	//Listener affecté au bouton +
+	class PlusListener implements ActionListener {
+		public void actionPerformed(ActionEvent arg0){
+			if(clicOperateur){
+				calcul();
+				ecran.setText(String.valueOf(chiffre1));
+			}
+			else{
+				chiffre1 = Double.valueOf(ecran.getText()).doubleValue();
+				clicOperateur = true;
+			}
+			operateur = "+";
+			update = true;
+		}
+	}
+
+	//Listener affecté au bouton -
+	class MoinsListener implements ActionListener {
+		public void actionPerformed(ActionEvent arg0){
+			if(clicOperateur){
+				calcul();
+				ecran.setText(String.valueOf(chiffre1));
+			}
+			else{
+				chiffre1 = Double.valueOf(ecran.getText()).doubleValue();
+				clicOperateur = true;
+			}
+			operateur = "-";
+			update = true;
+		}
+	}
+
+	//Listener affecté au bouton *
+	class MultiListener implements ActionListener {
+		public void actionPerformed(ActionEvent arg0){
+			if(clicOperateur){
+				calcul();
+				ecran.setText(String.valueOf(chiffre1));
+			}
+			else{
+				chiffre1 = Double.valueOf(ecran.getText()).doubleValue();
+				clicOperateur = true;
+			}
+			operateur = "*";
+			update = true;
+		}
+	}
+
+	//Listener affecté au bouton /
+	class DivListener implements ActionListener {
+		public void actionPerformed(ActionEvent arg0){
+			if(clicOperateur){
+				calcul();
+				ecran.setText(String.valueOf(chiffre1));
+			}
+			else{
+				chiffre1 = Double.valueOf(ecran.getText()).doubleValue();
+				clicOperateur = true;
+			}
+			operateur = "/";
+			update = true;
+		}
+	}
+
+	//Listener affecté au bouton de remise à zéro
+	class ResetListener implements ActionListener {
+		public void actionPerformed(ActionEvent arg0){
+			clicOperateur = false;
+			update = true;
+			chiffre1 = 0;
+			operateur = "";
+			ecran.setText("");
+		}
+	}
+
+
 }
---
+++ PooProject/src/Panels/PanelCalculette.java
@@ -0,0 +1,66 @@
+/*
+* Exercise W2Q3 - 2
+* Author: Clivaz Loris
+* Date creation: 16 mai 2018
+* 
+*/
+/**
+ * 
+ */
+package Panels;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import Images.IconBase;
+
+/**
+ * @author Loris_Clivaz
+ *
+ */
+public class PanelCalculette extends JPanel
+{
+
+	JLabel titrePanel;
+	Font globalFont = new Font("2.TimesRoman ",Font.BOLD,50);
+
+	IconBase vide = new IconBase("",40,40);
+
+	public String nomPhoto;
+
+	public PanelCalculette(String titre, String nomClass)
+	// TODO Auto-generated constructor stube
+	{
+		titrePanel = new JLabel(titre);
+		titrePanel.setFont(globalFont);
+
+		this.setPreferredSize(new Dimension(480, 78));
+		this.setBackground(Color.decode("#DFDFDF"));
+
+		this.setLayout(new FlowLayout(FlowLayout.CENTER,10,8)); 	//61 est la valeur maxs
+		if(nomClass.equals("ContactPanel")) {
+			//On met le plus à gauche
+			this.add(vide, BorderLayout.WEST);
+		}else 
+		//On met le titre au centre
+		this.add(titrePanel, BorderLayout.CENTER);
+
+		
+
+		//On met un listener sur le bouton
+//		create.addActionListener(new ClickCreate(nomClass));
+//		previous.addActionListener(new ClickPrevious());
+
+
+
+
+
+	}
+	
+}
