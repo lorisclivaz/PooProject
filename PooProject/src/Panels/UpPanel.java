93d5014e62c1c58282ee07ff3107364a16c87b11
Loris_Clivaz <Loris_Clivaz@Loris_Clivaz-HP>
2018-05-18 11:16:07 +0200

    @loris redéfinition de l'heure dans l'upanel

--- PooProject/src/Panels/UpPanel.java
+++ PooProject/src/Panels/UpPanel.java
@@ -1,9 +1,9 @@
 /*
-* Exercise W2Q3 - 2
-* Author: Clivaz Loris
-* Date creation: 2 mai 2018
-* 
-*/
+ * Exercise W2Q3 - 2
+ * Author: Clivaz Loris
+ * Date creation: 2 mai 2018
+ * 
+ */
 /**
  * 
  */
@@ -12,7 +12,9 @@ package Panels;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -20,6 +22,8 @@ import java.util.Date;
 
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.Timer;
+
 
 /**
  * @author Loris_Clivaz
@@ -32,28 +36,45 @@ public class UpPanel extends JPanel{
 	JLabel date = new JLabel();
 	DateFormat formatHeure = new SimpleDateFormat("HH:mm");
 	JLabel fourni = new JLabel("    Swisscom");
-	
 
-		//Constructeur qui définit la taille du panel ou il y aura la batterie l'heures ect...
-	
-		public UpPanel() 
-		{
-			this.setLayout(new BorderLayout(2,1));
-			this.setPreferredSize(new Dimension(480, 50));
-			this.setBackground(Color.BLACK);
-			
-			//heure
-			Calendar now = Calendar.getInstance();
-			
-			
-			date.setText("      4G     "+formatHeure.format(now.getTime())+"    ");
-			date.setForeground(Color.WHITE);
-			
-			
-			
-			fourni.setForeground(Color.WHITE);
-			this.add(fourni, BorderLayout.WEST);
-			this.add(date, BorderLayout.EAST);
-		}
+
+	// Heure
+	private JLabel heure = new JLabel();
+	final private DateFormat DATEFORMATHEURE = new SimpleDateFormat("HH:mm");
+	private Timer timer = new Timer(0, new CurrentTime());
+
+	//Constructeur qui définit la taille du panel ou il y aura la batterie l'heures ect...
+
+	public UpPanel() 
+	{
+		this.setLayout(new BorderLayout(2,1));
+		this.setPreferredSize(new Dimension(480, 50));
+		this.setBackground(Color.BLACK);
+
+		//heure
+		Calendar now = Calendar.getInstance();
+
+
+
+		// Heure
+		timer.start();
+		heure.setHorizontalAlignment(JLabel.CENTER);
+		heure.setFont(new Font("Arial", Font.BOLD, 15));
+		
+		heure.setForeground(Color.WHITE);
+		
+		fourni.setForeground(Color.WHITE);
+		this.add(fourni, BorderLayout.WEST);
+		this.add(heure, BorderLayout.EAST);
 	}
+
+	class CurrentTime implements ActionListener
+	{
+		@Override
+		public void actionPerformed(ActionEvent e) 
+		{
+			Calendar now = Calendar.getInstance();
+			heure.setText("      4G     "+DATEFORMATHEURE.format(now.getTime())+"    ");		}
+	}
+}
 
