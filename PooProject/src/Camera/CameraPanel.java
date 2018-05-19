0f3acb66e8ef4b7de0c7b71a6a806d5a6c319249
Loris_Clivaz <Loris_Clivaz@Loris_Clivaz-HP>
2018-05-19 18:45:11 +0200

    @loris création de la webcam

---
+++ PooProject/src/Camera/CameraPanel.java
@@ -0,0 +1,177 @@
+/*
+ * Exercise W2Q3 - 2
+ * Author: Clivaz Loris
+ * Date creation: 19 mai 2018
+ * 
+ */
+/**
+ * 
+ */
+package Camera;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.videoio.VideoCapture;
+
+import Panels.PanelCamera;
+
+/**
+ * @author Loris_Clivaz
+ *
+ */
+public class CameraPanel extends JPanel {
+	NewCenter center1= new NewCenter();
+	Timer timer;
+
+	PanelCamera panelcamera = new PanelCamera("Camera", "Camera");
+
+	public CameraPanel(){
+
+		setLayout(new BorderLayout());
+
+		this.add(panelcamera, BorderLayout.NORTH);
+		this.add(center1,BorderLayout.CENTER);
+		this.setBackground(new Color(0,191,255));
+
+		//setBounds(0, 0, 1280, 720);
+		this.setVisible(true);
+
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+
+
+
+		new MyThread().start();
+
+		timer = new Timer(5, new ActionListener() {
+
+			public void actionPerformed(ActionEvent e) {
+				//	ticktack();
+			}
+		});
+		timer.setRepeats(true);
+		timer.setCoalesce(true);
+		timer.setInitialDelay(0);
+		timer.start();
+
+		
+	}
+
+
+
+
+
+	public void opencam()
+	{
+		if(this.isVisible()==true)
+			center1.videoCap.openCam(0);
+		else
+			center1.repaint();
+	}
+
+
+
+	class MyThread extends Thread{
+		@Override
+		public void run() {
+			for (;;){
+				repaint();
+
+				try { Thread.sleep(10);
+				} catch (InterruptedException e) {    }
+			}  
+		} 
+	}
+
+}
+
+class NewCenter extends JPanel {
+
+	VideoCap videoCap = new VideoCap();
+	public NewCenter() {
+
+		setVisible(true);
+
+	}
+
+
+	public void paint(Graphics g){
+
+		g.drawImage(videoCap.getOneFrame(),0, 0, this.getWidth(), this.getHeight(), this);
+	}
+}
+
+
+class VideoCap {
+	static{
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+	}
+
+	VideoCapture cap;
+	Mat2Image mat2Img = new Mat2Image();
+
+	VideoCap(){
+		cap = new VideoCapture();
+		cap.open(0);
+
+	} 
+	void openCam(int i)
+	{
+		cap.open(i);
+
+	}
+	BufferedImage getOneFrame() {
+		cap.read(mat2Img.mat);
+		return mat2Img.getImage(mat2Img.mat);
+	}
+}
+class Mat2Image {
+	Mat mat = new Mat();
+	BufferedImage img;
+	byte[] dat;
+	public Mat2Image() {
+	}
+	public Mat2Image(Mat mat) {
+		getSpace(mat);
+	}
+	public void getSpace(Mat mat) {
+		this.mat = mat;
+		int w = mat.cols(), h = mat.rows();
+		if (dat == null || dat.length != w * h * 3)
+			dat = new byte[w * h * 3];
+		if (img == null || img.getWidth() != w || img.getHeight() != h
+				|| img.getType() != BufferedImage.TYPE_3BYTE_BGR)
+			img = new BufferedImage(w, h, 
+					BufferedImage.TYPE_3BYTE_BGR);
+	}
+	BufferedImage getImage(Mat mat){
+		getSpace(mat);
+		mat.get(0, 0, dat);
+		img.getRaster().setDataElements(0, 0, 
+				mat.cols(), mat.rows(), dat);
+		return img;
+	}
+	static{
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+	}
+}
--- PooProject/src/MainFrame/Frame.java
+++ PooProject/src/MainFrame/Frame.java
@@ -1,10 +1,10 @@
 package MainFrame;
 /*
-* Frame
-* Author: Clivaz Loris
-* Date creation: 30 avr. 2018
-* 
-*/
+ * Frame
+ * Author: Clivaz Loris
+ * Date creation: 30 avr. 2018
+ * 
+ */
 /**
  * 
  */
@@ -28,6 +28,7 @@ import javax.swing.JPanel;
 import javax.swing.Timer;
 
 import Calculette.Calculette;
+import Camera.CameraPanel;
 import ContactApp.ContactPanel;
 import GalerieApp.GalleryPanel;
 import Horloge.HorlogePanel;
@@ -52,7 +53,7 @@ public class Frame extends JFrame
 	JPanel mainpanel = new JPanel();
 	UpPanel uppanel = new UpPanel();
 	ImageFond imagefond = new ImageFond();
-	
+	CameraPanel camera = new CameraPanel();
 
 	private boolean lockFrame = false;
 
@@ -62,7 +63,7 @@ public class Frame extends JFrame
 
 	//Ajout des icons
 	IconLock iconlock = new IconLock();
-	
+
 
 	IconBase iconcontact = new IconBase("images/icones/contact.png", 60,60);
 	IconBase icongallery = new IconBase("images/icones/gallery.png", 60,60);
@@ -77,55 +78,55 @@ public class Frame extends JFrame
 	IconBase iconphoto = new IconBase("images/icones/photo.png",  60,60);
 	IconBase lockMain = new IconBase("images/icones/lockmain.png",  60,60);
 	IconBase vide ;
-	
-	
-	
+
+
+
 	//tri des panels static pour avoir accès dans le panel d'accueil
 	private     CardLayout cardLayout = new CardLayout();
 	private    JPanel triPanel = new JPanel(cardLayout);
-	
-	
-	
-	
+
+
+
+
 	public Frame() 
 	{
-		
-		
-		
+
+
+
 		this.setSize(480, 860);
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		this.setUndecorated(true);
 		this.setLocationRelativeTo(null);
 		this.setBackground(new Color(0, 0, 0));
 		this.setLayout(new BorderLayout());
-		
+
 		//ajout du panel du haut
 		this.add(uppanel, BorderLayout.NORTH);
-		
+
 
 		//ajout du panel du bas
 		this.add(backpanel, BorderLayout.SOUTH);
-		
+
 		//choisir la taille
 		backpanel.setPreferredSize(new Dimension(480, 80));
 		backpanel.setBackground(Color.BLACK);
 		backpanel.setLayout(new FlowLayout(30,165,10));
 		backpanel.add(iconlock);
 		iconlock.addActionListener(new ClickLock());
-		
-		
+
+
 		this.add(triPanel);
 		triPanel.add(verroupanel, "verroupanel");
 		triPanel.add(mainpanel, "mainpanel");
-		
+
 		mainpanel.setLayout(new BorderLayout());
 		mainpanel.setPreferredSize(new Dimension(480, 40));
 		mainpanel.add(imagefond, BorderLayout.CENTER);
-		
+
 		imagefond.setLayout(new FlowLayout(30,50,70));
-		
+
 		imagefond.add(icongallery);
-		
+
 		imagefond.add(iconmusic);
 		imagefond.add(iconcalculette);
 		imagefond.add(iconyoutube);
@@ -133,26 +134,28 @@ public class Frame extends JFrame
 		imagefond.add(iconinstagram);		
 		imagefond.add(iconagenda);
 		imagefond.add(iconphoto);
-		
-	
+
+
 		for (int i = 0; i < 8; i++) 
 		{
 			imagefond.add(new IconBase("images/icones/vide.png",  60,60));
 		}
-		
+
 		imagefond.add(iconpower);
 		imagefond.add(iconhorloge);
 		imagefond.add(iconcontact);
 		imagefond.add(lockMain );
 
-		
-		
-		
+
+
+
 		triPanel.add(gallerypanel, "gallerypanel");
 		triPanel.add(contactpanel,"contactpanel");
 		triPanel.add(calculette, "calculette");
 		triPanel.add(horlogepanel, "horlogepanel");
-		
+		triPanel.add(camera,"camera");
+
+
 		icongallery.addActionListener(new ClickGallery());
 		iconcontact.addActionListener(new ClickContact());	
 		iconpower.addActionListener(new ClickPower());
@@ -160,53 +163,58 @@ public class Frame extends JFrame
 		lockMain.addActionListener(new ClickLockMain());
 		iconhorloge.addActionListener(new ClickHorloge());
 		iconphoto.addActionListener(new ClickPhoto());
-		}
-	
+
+	}
+
+
+
 	private class ClickPhoto implements ActionListener
 	{
 
-		
+
 		@Override
 		public void actionPerformed(ActionEvent e)
 		{
-			
-			
+
+			System.out.println("clique camera");
+
+			cardLayout.show(triPanel, "camera");
 		}
-		
+
 	}
 	private class ClickHorloge implements ActionListener
 	{
 
-		
+
 		@Override
 		public void actionPerformed(ActionEvent e) 
 		{
 
 			cardLayout.show(triPanel, "horlogepanel");
 
-			
-			
+
+
 		}
-		
+
 	}
-	
-	
-	
-	
-	
+
+
+
+
+
 	class ClickLockMain implements ActionListener
 	{
 
-		
+
 		@Override
 		public void actionPerformed(ActionEvent arg0) 
 		{
 
 			cardLayout.show(triPanel, "verroupanel");
-			
+
 			lockFrame = false;
 		}
-		
+
 	}
 
 
@@ -214,14 +222,14 @@ public class Frame extends JFrame
 	class ClickCalculette implements ActionListener
 	{
 
-		
+
 		@Override
 		public void actionPerformed(ActionEvent e) {
 
-			
+
 			cardLayout.show(triPanel, "calculette");
 		}
-		
+
 	}
 
 
@@ -230,8 +238,8 @@ public class Frame extends JFrame
 	class ClickLock implements ActionListener
 	{
 
-		
-		
+
+
 		@Override
 		public void actionPerformed(ActionEvent e) {
 
@@ -241,202 +249,202 @@ public class Frame extends JFrame
 
 			}
 			System.out.println("clique bouton milieu");
-			
+
 
 		}
-		
+
 	}
 	//Quand on clique sur l'icon power
-	
-		class ClickPower implements ActionListener{
 
-			
-			
+	class ClickPower implements ActionListener{
+
+
+
+		@Override
+		public void actionPerformed(ActionEvent e) 
+		{
+
+			System.exit(0);
+
+		}
+
+	}
+
+	public class VerrouPanel extends JPanel
+	{
+
+		ImageFond imagefond = new ImageFond();
+		ImageFond imagefond2 = new ImageFond();
+		IconBase lock = new IconBase("images/icones/fingerprint.png", 70,70);
+
+		JPanel boutonlock = new JPanel();
+
+
+		// Date
+		private JLabel date = new JLabel();
+		final private DateFormat DATEFORMATDATE = new SimpleDateFormat("EEEE dd MMM");
+		private Timer timerDate = new Timer(0, new CurrentDate());
+
+		// Heure
+		private JLabel heure = new JLabel();
+		final private DateFormat DATEFORMATHEURE = new SimpleDateFormat("HH:mm");
+		private Timer timer = new Timer(0, new CurrentTime());
+
+		public VerrouPanel() 
+		{
+
+
+			this.setPreferredSize(new Dimension(480, 40));
+
+			this.setLayout(new BorderLayout());
+
+
+			imagefond.setLayout(new BorderLayout());
+
+			boutonlock.setLayout(new BorderLayout());
+			boutonlock.add(imagefond2);
+
+			imagefond2.setLayout(new FlowLayout(100,200,20));
+			imagefond2.add(lock);
+
+
+			lock.addActionListener(new ClickLock());
+
+
+			// Heure
+			timer.start();
+			imagefond.add(heure, BorderLayout.NORTH);
+			heure.setHorizontalAlignment(JLabel.CENTER);
+			heure.setForeground(Color.WHITE);
+			heure.setFont(new Font("Montserrat", Font.BOLD, 70));
+
+			// Date
+			timerDate.start();
+			imagefond.add(date, BorderLayout.CENTER);
+			date.setHorizontalAlignment(JLabel.CENTER);
+			date.setForeground(Color.WHITE);
+			date.setFont(new Font("Montserrat", Font.BOLD, 30));
+
+
+			this.add(imagefond, BorderLayout.CENTER);
+			this.add(boutonlock, BorderLayout.SOUTH);
+
+
+
+
+
+
+		}
+
+
+
+
+
+		private class ClickLock implements ActionListener
+		{
+
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+
+				cardLayout.show(triPanel, "mainpanel");
+				lockFrame = true;
+				System.out.println("cliqué");
+
+			}
+
+		}
+
+		private class CurrentDate implements ActionListener
+		{
+			@Override
+			public void actionPerformed(ActionEvent e)
+			{
+				Calendar now = Calendar.getInstance();
+				date.setText(DATEFORMATDATE.format(now.getTime()));
+			}
+		}
+
+		private class CurrentTime implements ActionListener
+		{
 			@Override
 			public void actionPerformed(ActionEvent e) 
 			{
-
-				System.exit(0);
-			
+				Calendar now = Calendar.getInstance();
+				heure.setText(DATEFORMATHEURE.format(now.getTime()));
 			}
-			
 		}
-		
-		public class VerrouPanel extends JPanel
+
+	}
+
+
+	//Quand on clique sur l'icon contacts
+
+	private class ClickContact implements ActionListener{
+
+
+		@Override
+		public void actionPerformed(ActionEvent arg0)
 		{
-
-			ImageFond imagefond = new ImageFond();
-			ImageFond imagefond2 = new ImageFond();
-			IconBase lock = new IconBase("images/icones/fingerprint.png", 70,70);
-
-			JPanel boutonlock = new JPanel();
-
-			
-			// Date
-			private JLabel date = new JLabel();
-			final private DateFormat DATEFORMATDATE = new SimpleDateFormat("EEEE dd MMM");
-			private Timer timerDate = new Timer(0, new CurrentDate());
-
-			// Heure
-			private JLabel heure = new JLabel();
-			final private DateFormat DATEFORMATHEURE = new SimpleDateFormat("HH:mm");
-			private Timer timer = new Timer(0, new CurrentTime());
-			
-			public VerrouPanel() 
-			{
-
-			
-				this.setPreferredSize(new Dimension(480, 40));
-				
-				this.setLayout(new BorderLayout());
-
-
-				imagefond.setLayout(new BorderLayout());
-				
-				boutonlock.setLayout(new BorderLayout());
-				boutonlock.add(imagefond2);
-
-				imagefond2.setLayout(new FlowLayout(100,200,20));
-				imagefond2.add(lock);
-
-
-				lock.addActionListener(new ClickLock());
-
-
-				// Heure
-				timer.start();
-				imagefond.add(heure, BorderLayout.NORTH);
-				heure.setHorizontalAlignment(JLabel.CENTER);
-				heure.setForeground(Color.WHITE);
-				heure.setFont(new Font("Montserrat", Font.BOLD, 70));
-
-				// Date
-				timerDate.start();
-				imagefond.add(date, BorderLayout.CENTER);
-				date.setHorizontalAlignment(JLabel.CENTER);
-				date.setForeground(Color.WHITE);
-				date.setFont(new Font("Montserrat", Font.BOLD, 30));
-				
-
-				this.add(imagefond, BorderLayout.CENTER);
-				this.add(boutonlock, BorderLayout.SOUTH);
-				
-				
-				
-				
-				
-				
-			}
-			
-			
-			
-			
-
-			private class ClickLock implements ActionListener
-			{
-
-				
-				@Override
-				public void actionPerformed(ActionEvent e) {
-
-					cardLayout.show(triPanel, "mainpanel");
-					lockFrame = true;
-					System.out.println("cliqué");
-					
-				}
-				
-			}
-			
-			private class CurrentDate implements ActionListener
-			{
-				@Override
-				public void actionPerformed(ActionEvent e)
-				{
-					Calendar now = Calendar.getInstance();
-					date.setText(DATEFORMATDATE.format(now.getTime()));
-				}
-			}
-			
-			private class CurrentTime implements ActionListener
-			{
-				@Override
-				public void actionPerformed(ActionEvent e) 
-				{
-					Calendar now = Calendar.getInstance();
-					heure.setText(DATEFORMATHEURE.format(now.getTime()));
-				}
-			}
-			
+			System.out.println("cliqué");
+			cardLayout.show(triPanel, "contactpanel");
 		}
 
-		
-		//Quand on clique sur l'icon contacts
+	}
 
-		private class ClickContact implements ActionListener{
+	//Quand on clique sur l'icon gallery
 
-			
-			@Override
-			public void actionPerformed(ActionEvent arg0)
-			{
-				System.out.println("cliqué");
-				cardLayout.show(triPanel, "contactpanel");
-			}
-			
-		}
-		
-		//Quand on clique sur l'icon gallery
-		
-		private class ClickGallery implements ActionListener{
+	private class ClickGallery implements ActionListener{
 
-			
-			@Override
-			public void actionPerformed(ActionEvent arg0) 
-			{			
-				
-				cardLayout.show(triPanel, "gallerypanel");
-				
-			}
-			
-		}
-		
-		
-		
-		
-		
-	
-		public void setImagefond(ImageFond imagefond) {
-			this.imagefond = imagefond;
+
+		@Override
+		public void actionPerformed(ActionEvent arg0) 
+		{			
+
+			cardLayout.show(triPanel, "gallerypanel");
+
 		}
 
-
-		
-		public CardLayout getCardLayout() {
-			return cardLayout;
-		}
+	}
 
 
 
 
 
-		public void setCardLayout(CardLayout cardLayout) {
-			this.cardLayout = cardLayout;
-		}
+
+	public void setImagefond(ImageFond imagefond) {
+		this.imagefond = imagefond;
+	}
+
+
+
+	public CardLayout getCardLayout() {
+		return cardLayout;
+	}
 
 
 
 
 
-		public JPanel getTriPanel() {
-			return triPanel;
-		}
+	public void setCardLayout(CardLayout cardLayout) {
+		this.cardLayout = cardLayout;
+	}
 
 
 
 
 
-		public void setTriPanel(JPanel triPanel) {
-			this.triPanel = triPanel;
-		}
-		
+	public JPanel getTriPanel() {
+		return triPanel;
+	}
+
+
+
+
+
+	public void setTriPanel(JPanel triPanel) {
+		this.triPanel = triPanel;
+	}
+
 }
 
