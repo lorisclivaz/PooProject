ebec1c843a086d294962bdd27e2dcaeb3b8cca2e
Loris_Clivaz <Loris_Clivaz@Loris_Clivaz-HP>
2018-05-28 10:34:16 +0200

    @loris Javadoc

--- PooProject/src/Calculette/Calculette.java
+++ PooProject/src/Calculette/Calculette.java
@@ -1,3 +1,8 @@
+/*
+ * Author : Vivian Bridy & Loris Clivaz
+ * Date creation : 14 mai 2018
+ */
+
 package Calculette;
 
 import java.awt.BorderLayout;
@@ -17,26 +22,35 @@ import javax.swing.JPanel;
 import Images.IconBase;
 import Panels.PanelSettings;
 
+
+/**
+ * Classe qui va gérer la calculette
+ * 
+ * Reprise sur le site OpenClassroom et remanié à notre sauce
+ * 
+ * @author Loris_Clivaz
+ *
+ *@lienInternet : https://openclassrooms.com/courses/apprenez-a-programmer-en-java/tp-une-calculatrice
+ */
+
 public class Calculette extends JPanel
 {
 
+	// Panel du haut
 	PanelSettings panelcalculette = new PanelSettings("Calculatrice", "Calculatrice");
 	IconBase point = new IconBase("images/icones/points.png", 50,50);
 
 
-
+	//Panel du centre
 	JPanel center = new JPanel();
 	JPanel bas = new JPanel();
 
-	//Création du panel sur le clicke
+	//Création du panel sur le click
 	JPanel panelCop = new JPanel();
-
-
 
 	//Tableau qui stocke les éléments de la calculatrice
 	String [] tab_string = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "0", ".", "=",
 			"C", "+", "-", "*", "/"};
-
 
 	//bouton
 	JButton [] tab_button = new JButton[tab_string.length];
@@ -48,35 +62,27 @@ public class Calculette extends JPanel
 	private double chiffre1;
 
 	private String operateur;
-	
-	//getter/setter
-	public void setEcran(JLabel ecran) {
-		this.ecran = ecran;
-	}
 
-
-	public void setChiffre1(double chiffre1) {
-		this.chiffre1 = chiffre1;
-	}
-
-
-	public void setOperateur(String operateur) {
-		this.operateur = operateur;
-	}
-
-	public JLabel getEcran() {
-		return ecran;
-	}
 
 
 
 	//choisir la dimension des boutons
 	private Dimension dim = new Dimension(60, 60);
-	
+
 
 	private Dimension dim2 = new Dimension(90, 90);
 
 	private boolean clicOperateur = false, update = false;
+
+	/**
+	 * Constructeur de la classe Calculette
+	 * 
+	 * - définit la taille du Panel et son layout
+	 * - ajoute et affiche les différents panels
+	 * -ajoute les coposants de la calculette
+	 * 
+	 * @author Loris
+	 */
 
 	public Calculette()
 	{
@@ -88,7 +94,7 @@ public class Calculette extends JPanel
 		//on rajoute le panel par defaut des apps dans le north du panel de base
 		this.add(panelcalculette, BorderLayout.NORTH);
 
-		
+
 
 		//on met le layout du centre pour l'ordre des panels
 		center.setLayout(new BorderLayout());
@@ -101,16 +107,26 @@ public class Calculette extends JPanel
 		//mets tous les composants dans le panel center
 		Composant();
 
-		
-		
+
+
 		this.add(center, BorderLayout.CENTER);
 		this.add(bas, BorderLayout.SOUTH);
 
-	
-		
-		
+
+
+
 	}
 
+	/**
+	 * Permet de créer les boutons de la calculette :
+	 * 
+	 * - Font pour les boutons
+	 * - Taille des boutons
+	 * -Ajout des actionlistener sur les boutons
+	 * -Ajout des composants sur le panel
+	 * 
+	 * @author Loris
+	 */
 
 	private void Composant() 
 	{
@@ -124,14 +140,14 @@ public class Calculette extends JPanel
 		//On aligne les informations à droite dans le JLabel
 		ecran.setHorizontalAlignment(JLabel.CENTER);
 		ecran.setPreferredSize(new Dimension(220, 40));
-		
+
 		JPanel operateur = new JPanel();      
 		operateur.setPreferredSize(new Dimension(200, 225));
-		
+
 		JPanel chiffre = new JPanel();
 		chiffre.setLayout(new GridLayout(4, 3,5,5));
 		chiffre.setPreferredSize(new Dimension(200, 225));
-		
+
 		JPanel panEcran = new JPanel();
 		panEcran.setPreferredSize(new Dimension(220, 60));
 
@@ -201,7 +217,14 @@ public class Calculette extends JPanel
 		center.add(operateur, BorderLayout.SOUTH);
 	}
 
-	//Méthode permettant d'effectuer un calcul selon l'opérateur sélectionné
+	/**
+	 * Méthode permettant d'effectuer un calcul selon l'opérateur sélectionné :
+	 * 
+	 * -Différentes conditions selon l'opérateur 
+	 * 
+	 * @author Loris
+	 */
+
 	public void calcul(){
 		if(operateur.equals("+")){
 			chiffre1 = chiffre1 + 
@@ -229,11 +252,17 @@ public class Calculette extends JPanel
 		}
 	}
 
-	
-	//Listener utilisé pour les chiffres
-	//Permet de stocker les chiffres et de les afficher
-	class ChiffreListener implements ActionListener {
+	/**
+	 * 
+	 * ActionListener qui se déclenche quand on clique sur un bouton
+	 * 
+	 * @author Loris
+	 *
+	 */
+	private class ChiffreListener implements ActionListener
+	{
 		public void actionPerformed(ActionEvent e){
+
 			//On affiche le chiffre additionnel dans le label
 			String str = ((JButton)e.getSource()).getText();
 			if(update){
@@ -247,8 +276,15 @@ public class Calculette extends JPanel
 		}
 	}
 
-	//Listener affecté au bouton =
-	class EgalListener implements ActionListener {
+	/**
+	 * 
+	 * ActionListener qui se déclenche quand on clique sur le bouton egal
+	 * 
+	 * @author Loris
+	 *
+	 */
+	private class EgalListener implements ActionListener 
+	{
 		public void actionPerformed(ActionEvent arg0){
 			calcul();
 			update = true;
@@ -256,8 +292,15 @@ public class Calculette extends JPanel
 		}
 	}
 
-	//Listener affecté au bouton +
-	class PlusListener implements ActionListener {
+	/**
+	 * 
+	 * ActionListener qui se déclenche quand on clique sur le bouton plus
+	 * 
+	 * @author Loris
+	 *
+	 */
+
+	private class PlusListener implements ActionListener {
 		public void actionPerformed(ActionEvent arg0){
 			if(clicOperateur){
 				calcul();
@@ -272,8 +315,14 @@ public class Calculette extends JPanel
 		}
 	}
 
-	//Listener affecté au bouton -
-	class MoinsListener implements ActionListener {
+	/**
+	 * 
+	 * ActionListener qui se déclenche quand on clique sur le bouton moins
+	 * 
+	 * @author Loris
+	 *
+	 */
+	private class MoinsListener implements ActionListener {
 		public void actionPerformed(ActionEvent arg0){
 			if(clicOperateur){
 				calcul();
@@ -288,8 +337,14 @@ public class Calculette extends JPanel
 		}
 	}
 
-	//Listener affecté au bouton *
-	class MultiListener implements ActionListener {
+	/**
+	 * 
+	 * ActionListener qui se déclenche quand on clique sur le bouton multiplier
+	 * 
+	 * @author Loris
+	 *
+	 */
+	private class MultiListener implements ActionListener {
 		public void actionPerformed(ActionEvent arg0){
 			if(clicOperateur){
 				calcul();
@@ -304,8 +359,14 @@ public class Calculette extends JPanel
 		}
 	}
 
-	//Listener affecté au bouton /
-	class DivListener implements ActionListener {
+	/**
+	 * 
+	 * ActionListener qui se déclenche quand on clique sur le bouton diviser
+	 * 
+	 * @author Loris
+	 *
+	 */
+	private class DivListener implements ActionListener {
 		public void actionPerformed(ActionEvent arg0){
 			if(clicOperateur){
 				calcul();
@@ -320,8 +381,14 @@ public class Calculette extends JPanel
 		}
 	}
 
-	//Listener affecté au bouton de remise à zéro
-	class ResetListener implements ActionListener {
+	/**
+	 * 
+	 * ActionListener qui se déclenche quand on clique sur le bouton remise à zéro
+	 * 
+	 * @author Loris
+	 *
+	 */
+	private class ResetListener implements ActionListener {
 		public void actionPerformed(ActionEvent arg0){
 			clicOperateur = false;
 			update = true;
@@ -331,5 +398,24 @@ public class Calculette extends JPanel
 		}
 	}
 
-	
+	//getter/setter
+	public void setEcran(JLabel ecran) {
+		this.ecran = ecran;
+	}
+
+
+	public void setChiffre1(double chiffre1) {
+		this.chiffre1 = chiffre1;
+	}
+
+
+	public void setOperateur(String operateur) {
+		this.operateur = operateur;
+	}
+
+	public JLabel getEcran() {
+		return ecran;
+	}
+
+
 }
